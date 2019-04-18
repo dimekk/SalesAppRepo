369e18b221944068879ebf8c6b9205055218ca7a
Wojciech Burczyk <wojciechburczyk@gmail.com>
2019-04-18 15:45:03 +0200

    Add files via upload

---
+++ build/precache-manifest.5a8681aafeaa18a57c04ac2105384a27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baa7e256b4c803e3cce4",
+    "url": "/SalesAppRepo/static/css/main.bd5b99e6.chunk.css"
+  },
+  {
+    "revision": "baa7e256b4c803e3cce4",
+    "url": "/SalesAppRepo/static/js/main.a2e394de.chunk.js"
+  },
+  {
+    "revision": "c4cf0d46c0426ea2ee18",
+    "url": "/SalesAppRepo/static/js/runtime~main.24e93913.js"
+  },
+  {
+    "revision": "f41a3b8043a1b7cf7490",
+    "url": "/SalesAppRepo/static/js/2.411e5cc8.chunk.js"
+  },
+  {
+    "revision": "42594fc5f527eac50fe71e34011b3695",
+    "url": "/SalesAppRepo/index.html"
+  }
+];
